bbec13ed04d7524d6783e6765f35065216350a49
jacksonhmg <jacksonmowattgok@gmail.com>
2022-11-10 17:04:49 +0800

    start q3

--- FinalAssessment_StudentID/Question3/Q3HashTable.java
+++ FinalAssessment_StudentID/Question3/Q3HashTable.java
@@ -1,12 +1,53 @@
 /**
  * DSA Final Assessment Question 3 - Q3HashTable.java
  *
- * Name : 
- * ID   :
+ * Name : Jackson Mowatt Gok
+ * ID   : 20568818
  *
  **/
+import java.util.*;
+import java.io.*;
 
 public class Q3HashTable {
+
+
+    public static void main(String[] args)
+    {
+        Q3HashTable hTable = new Q3HashTable(100);
+        readInHTable("6degrees.csv", hTable);
+        /*System.out.println(hTable.get(args[0]));*/
+    }
+
+
+
+    public static void readInHTable(String file, Q3HashTable hTable)
+    {
+        try{
+            File myObj = new File(file);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine())
+            {
+                String data = myReader.nextLine();
+                System.out.println(data);
+                String[] sArray = processLine(data);
+                hTable.resizeCheck();
+                hTable.put(sArray[0], sArray[1]);
+                //System.out.println(sArray[0]);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] processLine(String csvRow)
+    {  //reading one row of a file at a time, separated by string.split method
+        String[] splitLine;
+        splitLine = csvRow.split(","); 
+        return splitLine;
+    }
+
     
     protected class HashEntry
     {
@@ -98,6 +139,93 @@ public class Q3HashTable {
     }
 
 
+
+
+    public int stepHash(String inKey)
+    {
+        int key = 0;
+        for(int i = 0; i < inKey.length(); i++)
+        {
+            key += (int)inKey.charAt(i);
+        }
+        int max_step = 13;
+        int hashStep = max_step - (key % max_step);
+        return hashStep;
+    }
+
+    public Object get(String inKey)
+    {
+        int hashIdx = hash(inKey);
+        int origIdx = hashIdx;
+        boolean found = false;
+        boolean giveUp = false;
+
+        while(!found && !giveUp)
+        {
+            if(hashArray[hashIdx].state == 0)
+            {
+                giveUp = true;
+                //System.out.println("\nState equals zero");
+            }
+            else if(hashArray[hashIdx].key.equals(inKey))
+            {
+                found = true;
+            }
+            else
+            {
+                hashIdx = (hashIdx + stepHash(inKey));
+                if(hashIdx >= hashArray.length)
+                {
+                    hashIdx = hashIdx - hashArray.length;
+                }
+                if(hashIdx == origIdx)
+                {
+                    giveUp = true;
+                    //System.out.println("\n== orig");
+                }
+            }
+        }
+        if(!found)
+        {
+            throw new IllegalArgumentException("Can't find");
+        }
+        return hashArray[hashIdx].value;
+    }
+
+    public boolean hasKey(String inKey)
+    {
+        int hashIdx = hash(inKey);
+        int origIdx = hashIdx;
+        boolean found = false;
+        boolean giveUp = false;
+
+        while(!found && !giveUp)
+        {
+            if(hashArray[hashIdx].state == 0)
+            {
+                giveUp = true;
+            }
+            else if(hashArray[hashIdx].key.equals(inKey))
+            {
+                found = true;
+            }
+            else
+            {
+                hashIdx = (hashIdx + stepHash(inKey));
+                if(hashIdx >= hashArray.length)
+                {
+                    hashIdx = hashIdx - hashArray.length;
+                }
+                if(hashIdx == origIdx)
+                {
+                    giveUp = true;
+                }
+            }
+        }
+    
+        return found;
+    }
+
     public double getLoadFactor() {
         
         double loadFactor;
@@ -169,4 +297,80 @@ public class Q3HashTable {
 	{
         return hashArray.length;
     }
+
+
+    public void resizeCheck()
+    {
+        if(shouldResize() == 1)
+        {
+            growResize();
+        }
+        else if(shouldResize() == 2)
+        {
+            shrinkResize();
+        }
+    }
+
+
+    public int shouldResize()
+    {
+        int counter = 0;
+        for(int i = 0; i < hashArray.length; i++)
+        {
+           if(hashArray[i].state == 1)
+           {
+            counter++;
+           } 
+        }
+        if(counter >= (hashArray.length/2))
+        {
+            return 1;
+        }
+        if(counter <= (hashArray.length)/10)
+        {
+            return 2;
+        }
+        return 0;
+    }
+
+    public void growResize()
+    {
+        HashEntry[] tempArr = hashArray;
+        int actualSize = nextPrime((hashArray.length)*2);
+        hashArray = new HashEntry[actualSize];
+        //System.out.print(" GROWN TO " + hashArray.length + "!!!");
+        for(int i = 0; i<actualSize;i++)
+        {
+            hashArray[i] = new HashEntry();
+        }
+        for(int i = 0; i<tempArr.length;i++)
+        {
+            if(tempArr[i].state == 1)
+            {
+                put(tempArr[i].key, tempArr[i].value);
+            }
+        }
+    }
+
+    public void shrinkResize()
+    {
+        HashEntry[] tempArr = hashArray;
+        int actualSize = nextPrime((hashArray.length)/2);
+        hashArray = new HashEntry[actualSize];
+        //System.out.print(" SHRUNK TO " + hashArray.length + "!!!");
+        for(int i = 0; i<actualSize;i++)
+        {
+            hashArray[i] = new HashEntry();
+        }
+        for(int i = 0; i<tempArr.length;i++)
+        {
+            if(tempArr[i].state == 1)
+            {
+                put(tempArr[i].key, tempArr[i].value);
+            }
+        }
+    }
+
+
+
 } 
