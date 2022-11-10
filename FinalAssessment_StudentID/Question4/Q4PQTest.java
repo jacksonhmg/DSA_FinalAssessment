758c67fd4071d4b6c3a69562fd7a74439cf4de82
jacksonhmg <jacksonmowattgok@gmail.com>
2022-11-10 20:23:53 +0800

    mostly finished q4 test harness

--- FinalAssessment_StudentID/Question4/Q4PQTest.java
+++ FinalAssessment_StudentID/Question4/Q4PQTest.java
@@ -4,57 +4,105 @@ public class Q4PQTest {
     {
         // Creating empty priority queue
         PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
-  
-        // Adding items to the pQueue using add()
-        pQueue.add(10);
-        pQueue.add(20);
-        pQueue.add(15);
-  
-        // Printing the top element of PriorityQueue
-        System.out.println(pQueue.peek());
-  
-        // Printing the top element and removing it
-        // from the PriorityQueue container
-        System.out.println(pQueue.poll());
-  
-        // Printing the top element again
-        System.out.println(pQueue.peek());
-
-
-
-
-
                 
         int iNumPassed = 0;
         int iNumTests = 0;
 
         System.out.println("---------------------------------");
-        System.out.println("Testing All Shuffling Queue Features");
+        System.out.println("Testing All Queue Features");
 
-        // TEST 1 : Constructor
-        try
-        {
-            iNumTests ++;
-            System.out.print("Testing if new queue is empty: ");
-
-            iNumPassed++;
-            System.out.println("Passed!");
-        } catch(Exception e){
-            System.out.println("Failed!");
-        }
-
-        //TEST 2 : Enqueue
+        //TEST 1 : add
         try
         {
             iNumTests++;
-            System.out.print("Testing enqueue: ");
-            
+            System.out.print("Testing add: ");
+            pQueue.add(10);
+            pQueue.add(20);
+            pQueue.add(15);
             iNumPassed++;
             System.out.println("Passed!");
         } catch (Exception e){
             System.out.println("Failed!");
         }
 
+
+        // TEST 2 : Peek
+        try
+        {
+            iNumTests ++;
+            System.out.print("Testing peek method: ");
+            if(pQueue.peek() != 10)
+            {
+                throw new IllegalArgumentException();
+            }
+            iNumPassed++;
+            System.out.println("Passed!");
+        } catch(Exception e){
+            System.out.println("Failed!");
+        }
+
+        //TEST 3 : size
+        try
+        {
+            iNumTests++;
+            System.out.print("Testing size: ");
+            if(pQueue.size() != 3)
+            {
+                throw new IllegalArgumentException();
+            }
+            iNumPassed++;
+            System.out.println("Passed!");
+        } catch (Exception e){
+            System.out.println("Failed!");
+        }
+
+        //TEST 4 : Remove
+        try
+        {
+            iNumTests++;
+            System.out.print("Testing remove: ");
+            pQueue.remove(10);
+            if(pQueue.peek() != 15)
+            {
+                throw new IllegalArgumentException();
+            }
+            iNumPassed++;
+            System.out.println("Passed!");
+        } catch (Exception e){
+            System.out.println("Failed!");
+        }
+
+        //TEST 5 : Poll
+        try
+        {
+            iNumTests++;
+            System.out.print("Testing poll: ");
+            pQueue.poll();
+            if(pQueue.peek() != 20)
+            {
+                throw new IllegalArgumentException();
+            }
+            iNumPassed++;
+            System.out.println("Passed!");
+        } catch (Exception e){
+            System.out.println("Failed!");
+        }
+
+        //TEST 6 : isEmpty
+        try
+        {
+            iNumTests++;
+            System.out.print("Testing isEmpty: ");
+            pQueue.poll();
+            if(!pQueue.isEmpty())
+            {
+                throw new IllegalArgumentException();
+            }
+            iNumPassed++;
+            System.out.println("Passed!");
+        } catch (Exception e){
+            System.out.println("Failed!");
+        }
 
         System.out.println("Number PASSED: " + iNumPassed + "/" + iNumTests);
 
